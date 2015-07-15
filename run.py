f91b5efd1f2225deef453a694a0d9d2ea2798fa0
Juliusz Gonera <juliusz.gonera@airbnb.com>
2015-07-14 18:00:02 -0700

    Fix string

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ class Runner:
     POLL_INTERVAL = 5
     FRAME_FILE = "frame.jpg"
     STREAM_COMMAND = "avconv \
-        -f video4linux2 -i /dev/video0 -s 320x240 -r 30
-        -f mpeg1video -b 400k %s
+        -f video4linux2 -i /dev/video0 -s 320x240 -r 30 \
+        -f mpeg1video -b 400k %s \
         -f image2 -r 1/%i -an -update 1 %s"
 
     def __init__(self, url):
