d09f546534cac6239f6aab2e1d771e80f3c60e52
Juliusz Gonera <juliusz.gonera@airbnb.com>
2015-07-14 17:21:20 -0700

    Add first version of RPi script
    This will for sure not work, I can't even run it on my mac.

---
+++ run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from time import sleep
+from subprocess import Popen
+
+class Runner:
+    POLL_INTERVAL = 5
+    FRAME_FILE = "frame.jpg"
+    STREAM_COMMAND = "avconv \
+        -f video4linux2 -i /dev/video0 -s 320x240 -r 30
+        -f mpeg1video -b 400k %s
+        -f image2 -r 1/%i -an -update 1 %s"
+
+    def __init__(self, url):
+        self.url = url
+        self.stream_process = None
+
+    def run:
+        while True:
+            if !self.is_streaming():
+                self.start_stream()
+
+            sleep(POLL_INTERVAL)
+
+    def start_stream:
+        cmd = STREAM_COMMAND % (self.url, POLL_INTERVAL, FRAME_FILE)
+        print("Starting stream...")
+        print("Command: %s" % cmd)
+        self.stream_process = Popen(cmd, shell=True)
+
+    def is_streaming:
+        return self.stream_process != None and !self.stream_process.poll()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit('Usage: %s server_url' % sys.argv[0])
+
+    Runner(sys.argv[1]).run()
+
